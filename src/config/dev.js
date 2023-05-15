3edfa9ec510127126274cc48de20d5b7194a4ee8
Hassan Bouzlima <bouzlima.h@live.fr>
2023-05-15 18:02:11 +0100

    Add db and env config

---
+++ src/config/dev.js
@@ -0,0 +1,4 @@
+module.exports.envConfig = {
+  port: 3000,
+  dbUrl: "mongodb://localhost:27017/annuaire-places",
+};
---
+++ src/utils/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const { envConfig } = require("../config/dev");
+
+module.exports.connect = () => {
+  return mongoose.connect(envConfig.dbUrl);
+};
